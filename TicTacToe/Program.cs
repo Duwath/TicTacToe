26a615836bdd51cf4715d867fe5c8668035b683d
Duwath <tomschultekoeln@web.de>
2023-05-12 13:04:59 +0200

    Grundsätzliche logik

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -12,19 +12,25 @@ namespace TicTacToe
             string Player2 = "";
             Console.Write("Player 1 gib deinen Namen ein: ");
             Player1 = PlayerName();
-            Console.Write("Player 1 gib deinen Namen ein: ");
+            Console.Write("Player 2 gib deinen Namen ein: ");
             Player2 = PlayerName();
-            Console.Write("Heißer Scheiß! "+Player1+ " hat das Symbol X "+ Player2+" hat das Symbol O.\n");
-            ShowSpielfeld();
+            Console.Write("Heißer Scheiß! " + Player1 + " hat das Symbol X " + Player2 + " hat das Symbol O.\n");
+            ShowBspfeld();
             Console.Write("Ok. " + Player1 + " beginnt! Wähle eine Zahl um deine Position auf dem Spielfeld zu wählen.\n");
-
+            int Player1Number = 1;
+            int Player2Number = 2;
+            int[] Spielstand = { 0, 0, 0, 0, 0, 0, 0, 0, 0};
+            int SpielzugCounter = 1;
+            Game(Spielstand, SpielzugCounter, Player1, Player2);
+            
         }
+
         private static string PlayerName()
-                {
+        {
             return Console.ReadLine();
         }
-        
-        private static void ShowSpielfeld()
+
+        private static void ShowBspfeld()
         {
             string Leerzeile = "------------- ";
             string Zeile1 = "| 1 | 2 | 3 |";
@@ -40,5 +46,302 @@ namespace TicTacToe
             Console.WriteLine(Zeile3);
             Console.WriteLine(Leerzeile);
         }
+        private static void Game(int[]Spielstand, int SpielzugCounter, string Player1, string Player2)
+        {
+            string Auswahl = "";
+            Auswahl = Console.ReadLine();         
+            
+
+
+            switch (Auswahl)
+            {
+                case "1":
+                    if (SpielzugCounter  % 2 == 0)
+                    {
+                        if (Spielstand[0] == 0)
+                        {
+                            Spielstand[0] = 2;
+                            SpielzugCounter++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Dieses Feld ist schon von von einem Spieler belegt. Wähle ein anderes.");
+                            Game(Spielstand, SpielzugCounter, Player1, Player2);
+
+                        }
+                    }
+                    else
+                    {
+                        if (Spielstand[0] == 0)
+                        {
+                            Spielstand[0] = 1;
+                            SpielzugCounter++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Dieses Feld ist schon von von einem Spieler belegt. Wähle ein anderes.");
+                            Game(Spielstand, SpielzugCounter, Player1, Player2);
+
+                        }
+
+                    }                    
+                    break;
+                case "2":
+                    if (SpielzugCounter % 2 == 0)
+                    {
+                        if (Spielstand[1] == 0)
+                        {
+                            Spielstand[0] = 2;
+                            SpielzugCounter++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Dieses Feld ist schon von von einem Spieler belegt. Wähle ein anderes.");
+                            Game(Spielstand, SpielzugCounter, Player1, Player2);
+
+                        }
+                    }
+                    else
+                    {
+                        if (Spielstand[1] == 0)
+                        {
+                            Spielstand[0] = 1;
+                            SpielzugCounter++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Dieses Feld ist schon von von einem Spieler belegt. Wähle ein anderes.");
+                            Game(Spielstand, SpielzugCounter, Player1, Player2);
+
+                        }
+
+                    }
+                    break;
+                case "3":
+                    if (SpielzugCounter % 2 == 0)
+                    {
+                        if (Spielstand[2] == 0)
+                        {
+                            Spielstand[0] = 2;
+                            SpielzugCounter++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Dieses Feld ist schon von von einem Spieler belegt. Wähle ein anderes.");
+                            Game(Spielstand, SpielzugCounter, Player1, Player2);
+
+                        }
+                    }
+                    else
+                    {
+                        if (Spielstand[2] == 0)
+                        {
+                            Spielstand[0] = 1;
+                            SpielzugCounter++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Dieses Feld ist schon von einem Spieler belegt. Wähle ein anderes.");
+                            Game(Spielstand, SpielzugCounter, Player1, Player2);
+
+                        }
+
+                    }
+                    break;
+                case "4":
+                    if (SpielzugCounter % 2 == 0)
+                    {
+                        if (Spielstand[3] == 0)
+                        {
+                            Spielstand[0] = 2;
+                            SpielzugCounter++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Dieses Feld ist schon von von einem Spieler belegt. Wähle ein anderes.");
+                            Game(Spielstand, SpielzugCounter, Player1, Player2);
+
+                        }
+                    }
+                    else
+                    {
+                        if (Spielstand[3] == 0)
+                        {
+                            Spielstand[0] = 1;
+                            SpielzugCounter++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Dieses Feld ist schon von von einem Spieler belegt. Wähle ein anderes.");
+                            Game(Spielstand, SpielzugCounter, Player1, Player2);
+
+                        }
+
+                    }
+                    break;
+                case "5":
+                    if (SpielzugCounter % 2 == 0)
+                    {
+                        if (Spielstand[4] == 0)
+                        {
+                            Spielstand[0] = 2;
+                            SpielzugCounter++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Dieses Feld ist schon von von einem Spieler belegt. Wähle ein anderes.");
+                            Game(Spielstand, SpielzugCounter, Player1, Player2);
+
+                        }
+                    }
+                    else
+                    {
+                        if (Spielstand[4] == 0)
+                        {
+                            Spielstand[0] = 1;
+                            SpielzugCounter++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Dieses Feld ist schon von von einem Spieler belegt. Wähle ein anderes.");
+                            Game(Spielstand, SpielzugCounter, Player1, Player2);
+
+                        }
+
+                    }
+                    break;
+                case "6":
+                    if (SpielzugCounter % 2 == 0)
+                    {
+                        if (Spielstand[5] == 0)
+                        {
+                            Spielstand[0] = 2;
+                            SpielzugCounter++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Dieses Feld ist schon von von einem Spieler belegt. Wähle ein anderes.");
+                            Game(Spielstand, SpielzugCounter, Player1, Player2);
+
+                        }
+                    }
+                    else
+                    {
+                        if (Spielstand[5] == 0)
+                        {
+                            Spielstand[0] = 1;
+                            SpielzugCounter++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Dieses Feld ist schon von von einem Spieler belegt. Wähle ein anderes.");
+                            Game(Spielstand, SpielzugCounter, Player1, Player2);
+
+                        }
+
+                    }
+                    break;
+                case "7":
+                    if (SpielzugCounter % 2 == 0)
+                    {
+                        if (Spielstand[6] == 0)
+                        {
+                            Spielstand[0] = 2;
+                            SpielzugCounter++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Dieses Feld ist schon von von einem Spieler belegt. Wähle ein anderes.");
+                            Game(Spielstand, SpielzugCounter, Player1, Player2);
+
+                        }
+                    }
+                    else
+                    {
+                        if (Spielstand[6] == 0)
+                        {
+                            Spielstand[0] = 1;
+                            SpielzugCounter++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Dieses Feld ist schon von von einem Spieler belegt. Wähle ein anderes.");
+                            Game(Spielstand, SpielzugCounter, Player1, Player2);
+
+                        }
+
+                    }
+                    break;
+                case "8":
+                    if (SpielzugCounter % 2 == 0)
+                    {
+                        if (Spielstand[7] == 0)
+                        {
+                            Spielstand[0] = 2;
+                            SpielzugCounter++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Dieses Feld ist schon von von einem Spieler belegt. Wähle ein anderes.");
+                            Game(Spielstand, SpielzugCounter, Player1, Player2);
+
+                        }
+                    }
+                    else
+                    {
+                        if (Spielstand[7] == 0)
+                        {
+                            Spielstand[0] = 1;
+                            SpielzugCounter++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Dieses Feld ist schon von von einem Spieler belegt. Wähle ein anderes.");
+                            Game(Spielstand, SpielzugCounter, Player1, Player2);
+
+                        }
+
+                    }
+                    break;
+                case "9":
+                    if (SpielzugCounter % 2 == 0)
+                    {
+                        if (Spielstand[8] == 0)
+                        {
+                            Spielstand[0] = 2;
+                            SpielzugCounter++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Dieses Feld ist schon von von einem Spieler belegt. Wähle ein anderes.");
+                            Game(Spielstand, SpielzugCounter, Player1, Player2);
+
+                        }
+                    }
+                    else
+                    {
+                        if (Spielstand[8] == 0)
+                        {
+                            Spielstand[0] = 1;
+                            SpielzugCounter++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Dieses Feld ist schon von von einem Spieler belegt. Wähle ein anderes.");
+                            Game(Spielstand, SpielzugCounter, Player1, Player2);
+
+                        }
+
+                    }
+                    break;
+
+                default:
+                    Console.WriteLine("Diese Eingabe entspricht nicht den Zahlen von 1-9. Try again.");
+                    break;
+                    
+                    
+            }
+        }
     }
 }
+
